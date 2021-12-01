92fabca60b3013dc24e5356e47e68ea73be3b137
Devin Lafreniere <dlafreniere@coveo.com>
2021-12-01 18:06:36 -0500

    Day 1 - part 2

---
+++ src/day-1/part2.ts
@@ -0,0 +1,38 @@
+import { assert } from "console";
+import fs from "fs";
+import path from "path";
+
+const depths = fs
+  .readFileSync(path.resolve(__dirname, "input.txt"))
+  .toString()
+  .split("\n")
+  .map(d => parseInt(d, 10));
+assert(depths.length > 0);
+
+const WINDOW_SIZE = 3;
+
+const sum3 = (array: number[], start: number) =>
+  array[start] + array[start - 1] + array[start - 2];
+
+let increases = 0;
+
+const lines = [sum3(depths, WINDOW_SIZE-1) + " (N/A - no previous sum)"];
+for (let i = WINDOW_SIZE; i < depths.length; ++i) {
+  let currentSum = sum3(depths, i);
+  let previousSum = sum3(depths, i - 1);
+  let line = currentSum.toString();
+  if (currentSum > previousSum) {
+    ++increases;
+    line += " (increased)";
+  } else if (currentSum === previousSum) {
+    line += " (no change)";
+  } else {
+    line += " (decreased";
+  }
+  lines.push(line);
+}
+
+console.log(
+  `there are ${increases} sums that are larger than the previous sum`
+);
+fs.writeFileSync(path.resolve(__dirname, "output2.txt"), lines.join("\n"));
